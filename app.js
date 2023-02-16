ab43077d0662e6b006c0cad552fd6222c1512b35
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-02-16 11:18:11 +0100

    feature(swagger): add swagger config

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger.json");
+const swaggerDocument = require("./swagger-output.json");
 
 const usersRouter = require("./routes/api/users");
 
