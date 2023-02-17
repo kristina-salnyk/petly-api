f670d33dd6e6cf25478ec4846be587a4ad7d0594
Olga Plotkina <101660609+olga-plotkina@users.noreply.github.com>
2023-02-17 23:22:35 +0000

    fixbug/validateSchema

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 const { updateUser } = require("../../controllers/users.controller");
 const tryCatchWrapper = require("../../helpers/tryCatchWrapper");
-const validation = require("../../middlewares/validation");
+const { validateSchema } = require("../../middlewares/validation");
 const { updateUserSchema } = require("../../schemas/updateUserSchema");
 const { auth } = require("../../middlewares/auth");
 
-router.patch("/update", auth, validation(updateUserSchema), tryCatchWrapper(updateUser));
+router.patch("/update", auth, validateSchema(updateUserSchema), tryCatchWrapper(updateUser));
 
 module.exports = router;
