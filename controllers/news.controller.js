5b9c75c218e6baa598298e42fde28492a82381d1
maksim stryuts <maxim.stryuts@gmail.com>
2023-02-20 15:53:13 +0200

    .

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -1,12 +1,54 @@
 const { getNewses } = require("../services/news");
 
 const getNewsController = async (req, res, next) => {
+  /* 
+   #swagger.tags = ['News']
+    #swagger.summary = 'Get News'
+    #swagger.description = 'Get a list of news'
+
+  #swagger.parameters['page'] = {
+    in: 'query',
+    description: 'Page number to get news. Default is 1.',
+    required: false,
+    type: 'integer',
+    minimum: 1
+  }
+
+  #swagger.parameters['limit'] = {
+    in: 'query',
+    description: 'Maximum number of news per page. Default is 6.',
+    required: false,
+    type: 'integer',
+    minimum: 1,
+    maximum: 100
+  }
+
+
+*/
+
   console.log(1);
   const { page = 1, limit = 6 } = req.query;
   const skip = (page - 1) * limit;
 
   try {
     const news = await getNewses(skip, limit);
+    /*
+      #swagger.responses[200] = { 
+        description: 'List of news',
+        content: {
+          'application/json': {
+            schema: { $ref: '#/definitions/News' },
+            example: {
+                id: '615ec59c5bafc50b9a0c3f96',
+            title: 'Example News Article 1',
+            url: 'https://example.com/news/article1',
+            description: 'This is an example news article.',
+            date: '2021-10-07T00:00:00.000Z'
+            }
+          }
+        } 
+      }
+    */
     res.status(200).json(news);
   } catch (error) {
     next(error);
