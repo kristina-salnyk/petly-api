29e360e8130139ab253f29356d0320d055687e71
Olga Plotkina <101660609+olga-plotkina@users.noreply.github.com>
2023-02-21 20:24:31 +0000

    fix get notices by parameter

--- services/notices.js
+++ services/notices.js
@@ -1,4 +1,5 @@
 const { Notices } = require("../models/notice");
+const {User} = require("../models/user");
 
 const createNotice = async ({category, title, name, birthday, breed, gender, location, price, comments, image }, owner) => {
   return await Notices.create({category, title, name, birthday, breed, gender, location, price, comments, image, owner});
@@ -8,7 +9,21 @@ const getNoticeById = async (id) => {
   return await Notices.findById(id);
 };
 
+const getNoticeByCategory = async (category) => {
+  return await Notices.find({ category })
+
+};
+
+const getFavoriteNotices = async (_id) => {
+  const user = await User.findById(_id)
+    .populate("favorites", { _id: 0 })
+    .select("favorites");
+  return user.favorites;
+}
+
 module.exports = {
   createNotice,
-  getNoticeById
+  getNoticeById,
+  getNoticeByCategory,
+  getFavoriteNotices
 };
