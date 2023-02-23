a7c437237fbce3d11cfd294f959ebdfa5ced59a5
Olga Plotkina <101660609+olga-plotkina@users.noreply.github.com>
2023-02-23 10:13:59 +0000

    change way to get pets of owner

--- services/pets.js
+++ services/pets.js
@@ -1,39 +1,27 @@
-const { User } = require("../models/user");
 const { Pet } = require("../models/pet");
 const { BadRequest } = require("http-errors");
 
-const getPets = async id => {
-  return await User.findOne({ _id: id });
+const getPets = async owner => {
+  return await Pet.find({ owner });
 };
 
-const addPets = async ({ ...arg }, owner) => {
-  const petObj = await Pet.create({
+const addPet = async ({ ...arg }, owner) => {
+  return await Pet.create({
     ...arg,
     owner,
   });
-
-  await User.findByIdAndUpdate(
-    { _id: owner },
-    { $push: { userPets: petObj } },
-    {
-      new: true,
-    }
-  );
-  return petObj;
 };
 
-const removePets = async (petId, owner) => {
+const removePet = async petId => {
   const removeUserPet = await Pet.findByIdAndRemove(petId);
 
   if (!removeUserPet) {
     throw BadRequest(404, "Not found");
   }
-
-  await User.findByIdAndUpdate({ _id: owner }, { $pull: { userPets: petId } });
 };
 
 module.exports = {
   getPets,
-  addPets,
-  removePets,
+  addPet,
+  removePet,
 };
