309b7e0a86980508eed57fb26b40448dbd7b350e
maksim stryuts <maxim.stryuts@gmail.com>
2023-02-25 00:20:46 +0200

    done

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,7 +10,7 @@ const { auth } = require("../../middlewares/auth");
 router.patch(
   "/update",
   auth,
-  uploadCloud.single("avatarUrl"),
+  uploadCloud.single("avatarURL"),
   validateSchema(updateUserSchema),
   updateUser
 );
