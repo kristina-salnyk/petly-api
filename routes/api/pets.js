d033a1b4c36cad16b031ba7fb8b01a1d93d7799d
Olga Plotkina <101660609+olga-plotkina@users.noreply.github.com>
2023-02-26 23:32:26 +0000

    add validation

--- routes/api/pets.js
+++ routes/api/pets.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const { addPet, getPets, removePet } = require("../../controllers/pets.controller");
 const { auth } = require("../../middlewares/auth");
-// const { validateSchema } = require("../../middlewares/validation");
+const { validateSchema } = require("../../middlewares/validation");
 const { uploadCloud } = require("../../middlewares/uploadMiddleware");
-// const { petSchema } = require("../../schemas/petSchema");
+const { petSchema } = require("../../schemas/petSchema");
 
 router.get("/", auth, getPets);
-router.post("/", auth, uploadCloud.single("petImage"), addPet);
+router.post("/", auth, uploadCloud.single("petImage"), validateSchema(petSchema), addPet);
 router.delete("/:petId", auth, removePet);
 
 module.exports = router;
