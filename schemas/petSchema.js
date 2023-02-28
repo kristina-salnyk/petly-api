bd91c84eb7926974327de3d70299bc740c691b40
Olga Plotkina <101660609+olga-plotkina@users.noreply.github.com>
2023-02-28 11:39:52 +0000

    fix name validation

--- schemas/petSchema.js
+++ schemas/petSchema.js
@@ -2,7 +2,7 @@ const Joi = require("joi").extend(require("@joi/date"));
 
 const petSchema = Joi.object({
   name: Joi.string()
-    .pattern(/^[a-zA-Z]+$/)
+    .pattern(/^[a-zA-Z]*$/)
     .min(2)
     .max(16),
   birthday: Joi.date().format("YYYY-MM-DD"),
