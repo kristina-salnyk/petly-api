b9144b2032c282e144daa37470b3b1732295e113
Olga Plotkina <101660609+olga-plotkina@users.noreply.github.com>
2023-02-28 21:22:48 +0000

    put names of categories in model back

--- models/notice.js
+++ models/notice.js
@@ -5,7 +5,7 @@ const noticeSchema = Schema(
     category: {
       type: String,
       required: true,
-      // category: ["sell", "lost-found", "in-good-hands"],
+      category: ["sell", "lost-found", "in-good-hands"],
       // default: "sell",
     },
     title: {
