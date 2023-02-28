5d1940c58df7bdca9148d37400639dcd96ca7cf3
Olga Plotkina <101660609+olga-plotkina@users.noreply.github.com>
2023-02-28 14:13:47 +0000

    add sort to news

--- services/news.js
+++ services/news.js
@@ -2,6 +2,7 @@ const { News } = require("../models/news");
 
 const getNews = async (page = 1, limit = 6) => {
   return await News.find({})
+    .sort({ date: -1 })
     .skip((parseInt(page) - 1) * limit)
     .limit(limit);
 };
