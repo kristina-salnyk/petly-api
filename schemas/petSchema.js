9405f4695f3357e894fbd4a8cfb708d20f14eaed
Zubenko1 <102259832+Wensisn@users.noreply.github.com>
2023-02-28 20:59:35 +0200

    .

--- schemas/petSchema.js
+++ schemas/petSchema.js
@@ -10,10 +10,8 @@ const petSchema = Joi.object({
     .pattern(/^[a-zA-Z\s]*$/)
     .min(2)
     .max(16),
-  comments: Joi.string()
-    .min(8)
-    .max(120)
-    .pattern(/^[a-zA-Z\s]*$/),
+  comments: Joi.string().min(8).max(120),
+  // .pattern(/^[a-zA-Z\s]*$/),
   petImage: Joi.string(),
 });
 
