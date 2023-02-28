af7fb8379a7b85a275f1df8dab743a29aeabaf6b
Olga Plotkina <101660609+olga-plotkina@users.noreply.github.com>
2023-02-28 21:49:00 +0000

    add sort by date

--- services/notices.js
+++ services/notices.js
@@ -54,7 +54,7 @@ const getNoticeById = async id => {
 };
 
 const getNoticesByCategory = async category => {
-  return await Notices.find({ category });
+  return await Notices.find({ category }).sort("-date");
 };
 
 const getFavoriteNotices = async _id => {
