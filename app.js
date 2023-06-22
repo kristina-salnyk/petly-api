0a40baaeeff7bb4233b132581847367881ed304a
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-06-22 23:32:26 +0200

    feature(headers): add headers

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ const noticesRouter = require("./routes/api/notices");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://pet-support-frontend-beta.vercel.app");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
